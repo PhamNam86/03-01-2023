892a820e50eb9a81fac9c1d92f2702495508dc97
Pham_Nam <97910573+PhamNam86@users.noreply.github.com>
2023-01-03 16:09:51 +0700

    Add files via upload

---
+++ sketch_jan3b.ino
@@ -0,0 +1,35 @@
+#include<NewPing.h>// include newping library
+NewPing sonar (15, 15, 400);// create ultrasonic object with the following parameters (Trig=3, Echo=2, Max distance=400cm)
+float distance;// initialize a variable of type float
+
+NewPing sonar_2 (16, 16, 400);// create ultrasonic object with the following parameters (Trig=3, Echo=2, Max distance=400cm)
+float distance_2;// initialize a variable of type float
+
+NewPing sonar_3 (17, 17, 400);// create ultrasonic object with the following parameters (Trig=3, Echo=2, Max distance=400cm)
+float distance_3;// initialize a variable of type float
+void setup() {
+
+Serial.begin (115200);// initialize serial communication at 9600 bits per second
+
+}
+void loop(){
+
+distance = sonar.ping_cm();// putting the measured value in distance
+Serial.print("Distance_left = ");// print "Distance"
+Serial.print(distance);// print the value of the distance
+Serial.println(" cm");// print "cm"
+delay(10);
+
+distance_2 = sonar_2.ping_cm();// putting the measured value in distance
+Serial.print("Distance_front = ");// print "Distance"
+Serial.print(distance_2);// print the value of the distance
+Serial.println(" cm");// print "cm"
+delay(10);
+
+distance_3 = sonar_3.ping_cm();// putting the measured value in distance
+Serial.print("Distance_right = ");// print "Distance"
+Serial.print(distance_3);// print the value of the distance
+Serial.println(" cm");// print "cm"
+delay(1000);
+
+}
